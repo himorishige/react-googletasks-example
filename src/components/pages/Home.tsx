75d6726dc03842fbd1e46185aef1d92dc248409e
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-04 13:36:04 +0900

    update: Home component

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,33 @@
+import { List, Title, Loader } from '@mantine/core';
+import { Link } from 'react-router-dom';
+
 import { useApi } from '../../hooks/useApi';
 import { tasksRepository } from '../../repositories/tasksRepository';
 
 export const Home = () => {
-  const { data: tasks } = useApi(
-    ['taskLists', { maxResults: 5 }],
-    async ({ maxResults }, token) =>
-      tasksRepository.getTaskLists({ maxResults }, token),
+  const {
+    data: taskLists,
+    isLoading,
+    isError,
+  } = useApi(['taskLists', { maxResults: 5 }], async ({ maxResults }, token) =>
+    tasksRepository.getTaskLists({ maxResults }, token),
   );
-  console.log(tasks);
-  return <div>Home</div>;
+
+  if (isLoading) return <Loader />;
+  if (isError) return <div>Error</div>;
+
+  return (
+    <div>
+      <Title order={2}>Home</Title>
+
+      <List>
+        {taskLists &&
+          taskLists.map((taskList) => (
+            <List.Item key={taskList.id}>
+              <Link to={`lists/${taskList.id}/tasks`}>{taskList.title}</Link>
+            </List.Item>
+          ))}
+      </List>
+    </div>
+  );
 };
