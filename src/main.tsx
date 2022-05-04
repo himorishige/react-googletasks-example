d1147f0ec0778cc51da55d78840439f5dcae0d3a
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-04 12:00:02 +0900

    add: AuthGuardProvider

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,16 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
 import './index.css';
 import { queryClient } from './libs/queryClient';
+import { AuthGuard } from './providers/AuthGuard';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <AuthGuard>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </AuthGuard>
   </React.StrictMode>,
 );
