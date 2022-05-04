ff6912c34be321094e77c3b5e8a85aba190666a7
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-04 21:43:47 +0900

    add: custom usePosts hooks

---
+++ src/hooks/useTasks.ts
@@ -0,0 +1,26 @@
+import { tasksRepository } from '../repositories/tasksRepository';
+import { useApi } from './useApi';
+
+export const useTasksApi = () => {
+  const useFetchTaskLists = () =>
+    useApi(['taskLists', { maxResults: 5 }], async ({ maxResults }, token) =>
+      tasksRepository.getTaskLists({ maxResults }, token),
+    );
+
+  const useFetchTaskList = (taskListId: string) =>
+    useApi(
+      ['tasks', { taskListId }],
+      async ({ taskListId }, token) =>
+        tasksRepository.getTasks({ taskListId }, token),
+      { enabled: !!taskListId },
+    );
+
+  const useFetchTasks = (taskListId: string, taskId: string) =>
+    useApi(
+      ['task', { taskListId, taskId }],
+      async (params, token) => tasksRepository.getTask(params, token),
+      { enabled: !!taskId && !!taskListId },
+    );
+
+  return { useFetchTaskLists, useFetchTaskList, useFetchTasks };
+};
