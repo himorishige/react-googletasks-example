9107c7045017721d621adad5767a9e7898875f8b
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-04 12:00:18 +0900

    add: tasks types

---
+++ src/types/tasks/index.ts
@@ -0,0 +1,45 @@
+export type TaskList = {
+  kind: string;
+  id: string;
+  etag: string;
+  title: string;
+  updated: string;
+  selfLink: string;
+};
+
+export type TaskListsResponse = {
+  kind: string;
+  etag: string;
+  items: TaskList[];
+};
+
+export type Task = {
+  kind: string;
+  id: string;
+  etag: string;
+  title: string;
+  updated: string;
+  selfLink: string;
+  parent: string;
+  position: string;
+  notes: string;
+  status: string;
+  due: string;
+  completed: string;
+  deleted: boolean;
+  hidden: boolean;
+  links: Link[];
+};
+
+type Link = {
+  type: string;
+  description: string;
+  link: string;
+};
+
+export type TasksResponse = {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  items: Task[];
+};
