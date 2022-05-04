0c31e3bfd9b5ee47a9b55a64ef2e97a8e271a383
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-04 13:36:17 +0900

    add: Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Title } from '@mantine/core';
+import { Routes, Route, Link } from 'react-router-dom';
 
-import './App.css';
 import { Home } from './components/pages/Home';
+import { TaskList } from './components/pages/TaskList';
 
 function App() {
   return (
     <div className="App">
+      <Title order={1}>
+        <Link to="/">Tasks</Link>
+      </Title>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="lists">
+          <Route path=":taskListId/tasks" element={<TaskList />} />
+        </Route>
       </Routes>
     </div>
   );
