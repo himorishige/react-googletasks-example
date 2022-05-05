4cd62b223d5794a785f8aae460f1404a12c411f0
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-05 15:12:37 +0900

    add: default options

--- src/libs/queryClient.ts
+++ src/libs/queryClient.ts
@@ -3,7 +3,14 @@ import { QueryClient } from 'react-query';
 export function generateQueryClient(): QueryClient {
   return new QueryClient({
     defaultOptions: {
-      queries: {},
+      queries: {
+        retry: false,
+        staleTime: 300000,
+        cacheTime: 300000,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false,
+      },
       mutations: {},
     },
   });
