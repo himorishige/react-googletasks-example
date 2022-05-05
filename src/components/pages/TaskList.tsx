5b59d145a7a5deea0dc6d7388e238243d7a86972
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-05 12:59:21 +0900

    update: TaskListPage

--- src/components/pages/TaskList.tsx
+++ src/components/pages/TaskList.tsx
@@ -1,20 +1,13 @@
 import { Button, List, Loader, TextInput, Title } from '@mantine/core';
 import { useForm } from '@mantine/form';
-import { useQueryClient } from 'react-query';
 import { Link, useParams } from 'react-router-dom';
 import invariant from 'tiny-invariant';
 
-import { useMutateWrapper } from '../../hooks/useApi';
 import { useTasksApi } from '../../hooks/useTasks';
-import { tasksRepository } from '../../repositories/tasksRepository';
-
-import type { Task } from '../../types/tasks';
 
 export const TaskList = () => {
   const { taskListId } = useParams();
   invariant(taskListId, 'taskListId is required');
-
-  const queryClient = useQueryClient();
 
   const form = useForm({
     initialValues: {
@@ -25,46 +18,24 @@ export const TaskList = () => {
     },
   });
 
-  const createTask = useMutateWrapper<Partial<Task>, Task[]>(
-    async (params, token) =>
-      tasksRepository.createTask({ ...params, taskListId }, token),
-    {
-      onMutate: async (params) => {
-        await queryClient.cancelQueries(['tasks', { taskListId }]);
-
-        const previousTasks = queryClient.getQueryData<Task[]>([
-          'tasks',
-          { taskListId },
-        ]);
-
-        if (previousTasks) {
-          queryClient.setQueryData<Partial<Task>[]>(
-            ['tasks', { taskListId }],
-            [{ title: params.title }, ...previousTasks],
-          );
-        }
-
-        return previousTasks;
-      },
-      onError: (error, _, context) => {
-        if (context && context.length > 0) {
-          queryClient.setQueryData<Task[]>(['tasks', { taskListId }], context);
-        }
-        console.log(error);
-      },
-      onSettled: () => {
-        queryClient.invalidateQueries(['tasks', { taskListId }]);
-      },
-    },
-  );
-
   type FormValues = typeof form.values;
-  const submitHandler = (values: FormValues) => {
-    createTask.mutate({ title: values.title });
-  };
 
-  const { useFetchTaskList } = useTasksApi();
+  const { useFetchTaskList, useAddTask } = useTasksApi();
+
   const { data: tasks, isLoading, isError } = useFetchTaskList(taskListId);
+
+  const createTask = useAddTask(taskListId);
+
+  const submitHandler = async (values: FormValues) => {
+    try {
+      await createTask.mutateAsync({
+        id: Math.random().toString(),
+        title: values.title,
+      });
+    } catch (e) {
+      console.warn(e);
+    }
+  };
 
   if (isLoading) return <Loader />;
   if (isError) return <div>Error</div>;
