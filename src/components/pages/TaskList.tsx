404406a75f1b265f9a30e1650e516a3234e5bf13
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-05 21:10:55 +0900

    add: updateTask

--- src/components/pages/TaskList.tsx
+++ src/components/pages/TaskList.tsx
@@ -1,14 +1,14 @@
-import { Button, List, Loader, TextInput, Title } from '@mantine/core';
+import { Button, Grid, Loader, Stack, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useCallback } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { TrashX } from 'tabler-icons-react';
+import { useParams } from 'react-router-dom';
+import { SquarePlus } from 'tabler-icons-react';
 import invariant from 'tiny-invariant';
 
 import { useTasksApi } from '../../hooks/useTasks';
+import { TaskListItem } from '../models/tasks/TaskListItem';
 
 import type { Task } from '../../types/tasks';
-import type { FC } from 'react';
 
 export const TaskList = () => {
   const { taskListId } = useParams();
@@ -25,8 +25,13 @@ export const TaskList = () => {
 
   type FormValues = typeof form.values;
 
-  const { useFetchTaskList, useAddTask, useDeleteTask, usePrefetchTask } =
-    useTasksApi();
+  const {
+    useFetchTaskList,
+    useAddTask,
+    useUpdateTask,
+    useDeleteTask,
+    usePrefetchTask,
+  } = useTasksApi();
 
   const { data: tasks, isLoading, isError } = useFetchTaskList(taskListId);
 
@@ -48,6 +53,19 @@ export const TaskList = () => {
     }
   };
 
+  const updateTask = useUpdateTask(taskListId);
+
+  const completeHandler = useCallback(
+    async (taskId: string, params: Partial<Task>) => {
+      try {
+        await updateTask.mutateAsync({ taskId, ...params });
+      } catch (e) {
+        console.warn(e);
+      }
+    },
+    [updateTask],
+  );
+
   const deleteTask = useDeleteTask(taskListId);
 
   const deleteHandler = useCallback(
@@ -65,70 +83,46 @@ export const TaskList = () => {
   if (isError) return <div>Error</div>;
 
   return (
-    <div>
-      <Title order={2}>TaskList {taskListId}</Title>
+    <>
+      <Stack>
+        <Stack spacing="sm">
+          {tasks &&
+            tasks.map((task) => (
+              <TaskListItem
+                key={task.id}
+                taskListId={taskListId}
+                task={task}
+                deleteHandler={deleteHandler}
+                completeHandler={completeHandler}
+                isDeleting={deleteTask.isLoading}
+                isCreating={createTask.isLoading}
+                usePrefetchTask={usePrefetchTask}
+              />
+            ))}
+        </Stack>
 
-      <List>
-        {tasks &&
-          tasks.map((task) => (
-            <TaskItem
-              key={task.id}
-              taskListId={taskListId}
-              task={task}
-              deleteHandler={deleteHandler}
-              isDeleting={deleteTask.isLoading}
-              isCreating={createTask.isLoading}
-              usePrefetchTask={usePrefetchTask}
-            />
-          ))}
-      </List>
-
-      <form onSubmit={form.onSubmit((values) => submitHandler(values))}>
-        <TextInput required label="title" {...form.getInputProps('title')} />
-        <Button type="submit" disabled={createTask.isLoading}>
-          Add task
-        </Button>
-      </form>
-    </div>
-  );
-};
-
-type TaskItemProps = {
-  taskListId: string;
-  task: Pick<Task, 'id' | 'title'>;
-  deleteHandler: (taskId: string) => Promise<void>;
-  isDeleting: boolean;
-  isCreating: boolean;
-  usePrefetchTask: (taskListId: string, taskId: string) => () => void;
-};
-
-const TaskItem: FC<TaskItemProps> = ({
-  taskListId,
-  task,
-  deleteHandler,
-  isCreating,
-  isDeleting,
-  usePrefetchTask,
-}) => {
-  const prefetchTask = usePrefetchTask(taskListId, task.id);
-
-  const prefetchHandler = () => {
-    prefetchTask();
-  };
-
-  return (
-    <List.Item key={task.id}>
-      <Link to={`${task.id}`}>{task.title}</Link>
-      <Button
-        compact
-        onClick={() => deleteHandler(task.id)}
-        disabled={isCreating || isDeleting}
-      >
-        <TrashX size={12} />
-      </Button>
-      <Button compact onClick={prefetchHandler}>
-        Prefetch
-      </Button>
-    </List.Item>
+        <form onSubmit={form.onSubmit((values) => submitHandler(values))}>
+          <Grid grow gutter="sm">
+            <Grid.Col span={9}>
+              <TextInput
+                required
+                {...form.getInputProps('title')}
+                placeholder="タスクを登録"
+              />
+            </Grid.Col>
+            <Grid.Col span={3}>
+              <Button
+                type="submit"
+                fullWidth
+                leftIcon={<SquarePlus />}
+                loading={createTask.isLoading}
+              >
+                Add task
+              </Button>
+            </Grid.Col>
+          </Grid>
+        </form>
+      </Stack>
+    </>
   );
 };
